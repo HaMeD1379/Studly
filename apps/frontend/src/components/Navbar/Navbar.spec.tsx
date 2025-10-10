bc0080e27f6f202a5232a37105ebaec871782c10
xxBennny <benpro100@gmail.com>
2025-10-09 21:30:58 -0500

    tested navbar

--- apps/frontend/src/components/Navbar/Navbar.spec.tsx
+++ apps/frontend/src/components/Navbar/Navbar.spec.tsx
@@ -1,7 +1,38 @@
-import { expect, describe, it } from 'vitest'
+const mockUseNavigation = vi.fn();
+
+vi.mock('react-router', () => ({
+    useNavigate: () => mockUseNavigation.mockImplementation(() => {}),
+    useLocation: () => vi.fn().mockImplementation(() => {})
+}));
+
+import { expect, describe, it, vi } from 'vitest'
+import { screen } from '@testing-library/react';
+import { render } from '~/utilities/testing';
+import { Navbar } from './Navbar';
 
 describe('Navbar', () => {
-  it('true', () => {
-    expect(1).toEqual(1);
+  beforeEach(() => {
+    vi.resetAllMocks();
   });
-})
+
+  it('renders all navigations', () => {
+    render(<Navbar>MOCK_CHILDREN</Navbar>);
+    expect(screen.getByText('Studly')).not.toBeNull();
+    expect(screen.getByRole('button', { name: 'Home' })).not.toBeNull();
+    expect(screen.getByRole('button', { name: 'Study Session' })).not.toBeNull();
+    expect(screen.getByText('MOCK_CHILDREN')).not.toBeNull();
+  });
+
+  it('navigations are called to the proper route', () => {
+    render(<Navbar>MOCK_CHILDREN</Navbar>);
+
+    const homeButton = screen.getByRole('button', { name: 'Home' });
+    const studySessionButton = screen.getByRole('button', { name: 'Study Session' });
+
+    homeButton.click();
+    expect(mockUseNavigation).toHaveBeenCalledWith('/');
+
+    studySessionButton.click();
+    expect(mockUseNavigation).toHaveBeenCalledWith('/study');
+  });
+});
