75668f18f17327b939cd3462605f9229062d574b
Anthony <okoliea1@myumanitoba.ca>
2025-10-26 23:21:13 -0500

    Refactoring to solve merge conflicts and follow coding conventions

--- apps/frontend/src/utilities/authentication/SupabaseClient.ts
+++ apps/frontend/src/utilities/authentication/SupabaseClient.ts
@@ -1,10 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL! ?? "mock-url";
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY! ?? "mock-key";
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL ?? "mock-url";
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY ?? "mock-key";
 
-if (!supabaseUrl || !supabaseAnonKey) {
-  throw new Error("Missing Supabase environment variables.");
-}
+
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
