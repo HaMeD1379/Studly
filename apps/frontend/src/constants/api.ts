6ac8a3eb83095f9e94cfbd5074b30bb25fdb5bd6
Anthony <okoliea1@myumanitoba.ca>
2025-11-03 14:27:33 -0600

    updates the api constant file to add endpoints

--- apps/frontend/src/constants/api.ts
+++ apps/frontend/src/constants/api.ts
@@ -7,6 +7,9 @@ export const API_KEY_HEADER = 'x-api-key';
 export const CONTENT_TYPE = 'application/json';
 
 export const AUTH_SIGNUP = 'auth/signup';
+export const AUTH_LOGIN = 'auth/login';
+export const AUTH_FORGOTPASSWORD = 'auth/forgot-password';
+export const AUTH_LOGOUT = 'auth/logout';
 
 export const SESSIONS_SUMMARY = 'sessions/summary';
 export const SESSIONS = 'sessions';
