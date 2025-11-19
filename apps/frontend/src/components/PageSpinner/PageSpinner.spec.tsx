14d8e2517f56ab2f2ebc1b37e9f00f7dff756738
xxBennny <benpro100@gmail.com>
2025-11-18 20:24:18 -0600

    unused import

--- apps/frontend/src/components/PageSpinner/PageSpinner.spec.tsx
+++ apps/frontend/src/components/PageSpinner/PageSpinner.spec.tsx
@@ -1,5 +1,5 @@
 import { screen } from '@testing-library/react';
-import { describe, expect, it, vi } from 'vitest';
+import { describe, expect, it } from 'vitest';
 import { render } from '~/utilities/testing';
 import { PageSpinner } from './PageSpinner';
 
